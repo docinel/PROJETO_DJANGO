7403e4251635d519ded9257091bd39724c712468
Rodrigo Docinel <docinel@gmail.com>
2022-07-02 17:36:17 -0300

    Created Home.html

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'home.thml')
 
 def contato(request):
     return HttpResponse('contato')
