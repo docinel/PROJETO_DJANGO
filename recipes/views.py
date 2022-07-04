9ac5ceb349ae3db74bebf975bbe5d8f0fdb17867
Rodrigo Docinel <docinel@gmail.com>
2022-07-03 23:33:28 -0300

    Correting html, urls...

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.thml')
+    return render(request, 'home.html')
 
 def contato(request):
     return HttpResponse('contato')
