acda2a07e4290785664dc08ee87f19dec237166a
Rodrigo Docinel <docinel@gmail.com>
2022-07-04 21:15:41 -0300

    Removed contcts and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Rodrigo',
     })
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
